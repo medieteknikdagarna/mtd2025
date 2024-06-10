e6ec0ae0c176b37751ccb804bdea00bc0494ebf9
Max Wiklundh <wiklundhmax@gmail.com>
2024-06-10 14:43:10 +0200

    test

--- src/components/pocketbase/pockethost.js
+++ src/components/pocketbase/pockethost.js
@@ -3,3 +3,7 @@ import PocketBase from 'pocketbase';
 const url = 'https://mtd2024-databas.pockethost.io/'
 
 export const pocketbaseClient = new PocketBase(url)
+
+const records = await client.collection('posts').getFullList({
+    sort: '-created',
+});
--- src/pages/booking.js
+++ src/pages/booking.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 //import SeatBooker from "@/components/SeatBooker";
 import BookingFormV2 from "@/components/BookingFormV2";
 import Footer from "@/components/Footer";
+//import BookingFormV3 from "@/components/BookingFormV3";
 
 
 // Sida: https://www.medieteknikdagen.se/booking
@@ -12,13 +13,25 @@ import Footer from "@/components/Footer";
 // Kolla andra krav från företag
 // varför kan du klicka på de röda platserna
 
+
+//flytta
+// import PocketBase from 'pocketbase';
+
+// const url = 'https://mtd2024-databas.pockethost.io/'
+
+// const pocketbaseClient = new PocketBase(url)
+// //const records = await 
+
+
 export default function BookingPage() {
   const [type, setType] = useState("Mässplats");
+
   return (
     <>
       <NextSeo noindex={true} />
       <Header changeOnScroll />
-      <BookingFormV2 />
+      <BookingFormV2 /> 
+      {/* <BookingFormV3 /> */}
       <Footer />
     </>
   );
