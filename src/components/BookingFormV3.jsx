98ec10b8e552272562e15f827fb0eea51e4799d8
Max Wiklundh <wiklundhmax@gmail.com>
2024-06-27 16:47:05 +0200

    test

--- src/components/BookingFormV3.jsx
+++ src/components/BookingFormV3.jsx
@@ -83,7 +83,7 @@ export default function BookingFormV3() {
 
   const emailMessage = () => {
     var mailOptions = {
-      from: process.env.NEXT_PUBLIC_EMAIL_USER,
+      from: 'info@medieteknikdagarna.se',
       to: formValues.email,
       subject: 'MTD2024 Anmälan',
       text: 'Hej, tack för din anmälan till Medieteknikdagarna 2024! Vi kommer att kontakta dig inom kort för att bekräfta din anmälan och hantera underskrift. Med vänliga hälsningar, Medieteknikdagarna 2024'
@@ -306,8 +306,8 @@ export default function BookingFormV3() {
     //const authData = await pb.collection('users').authWithPassword('Test', 'database1TestPass');
 
     pb.autoCancellation(false);
-
-    const authData = await pb.admins.authWithPassword(NEXT_PUBLIC_POCKETHOST_ADMIN, NEXT_PUBLIC_POCKETHOST_PASS);
+    console.log(process.env.NEXT_PUBLIC_POCKETHOST_ADMIN);
+    const authData = await pb.admins.authWithPassword('webb@medieteknikdagarna.se', 'mtdWEBB2024!');
 
     console.log(authData);
 
