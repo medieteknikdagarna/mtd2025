5674bd17c6a9cd86de4b00f88122de4290285cfe
Max Wiklundh <wiklundhmax@gmail.com>
2024-06-29 22:04:23 +0200

    test6

--- src/components/newFormStuff/SeatMap2.js
+++ src/components/newFormStuff/SeatMap2.js
@@ -101,6 +101,10 @@ export default function SeatMap({ seats, setType, reservations, activeFloor, typ
       element.classList.remove("seat-inactive");  // does not do anything important
       element.classList.remove("seat-active");   // does not do anything important
       element.classList.remove("seat-animation");
+      element.removeEventListener("click", handleClick, false);
+      element.removeEventListener("click", handleClick);
+      element.removeEventListener("click", handleClick);
+      element.removeEventListener("click", handleClick);
       element.removeEventListener("click", handleClick);
       element.removeEventListener("click", handleClick);
 
