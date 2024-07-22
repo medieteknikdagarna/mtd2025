862b58daf2ed33e4799b0cc82d54e4015a481938
Max Wiklundh <wiklundhmax@gmail.com>
2024-07-22 23:32:31 +0200

    expo temp

--- src/pages/studentEXPO.jsx
+++ src/pages/studentEXPO.jsx
@@ -3,21 +3,35 @@ import ExpoCard from "@/components/EXPO-components/ExpoCard";
 import Content from "public/content/EXPO-content/expo-projects.json";
 import StarsCanvas from "@/components/EXPO-components/StarsCanvas";
 import { useTypewriter, Typewriter } from "react-simple-typewriter";
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { languageContext } from "./_app";
+
 
 // Används inte längre (?)
 const StudentEXPO = () => {
   //console.log(Content.sv.uställare[0]);
   const [doneTyping, setDoneTyping] = useState(false);
+  const [lang, setLang] = useContext(languageContext);
+
 
   const HandleDoneTyping = () => {
     setDoneTyping(true);
     console.log("done");
   };
+  let titleText = lang === "sv" ? "Årets utställare på studentEXPO!" : "Exhibitor of the year at studentEXPO!"
+
   const [Text] = useTypewriter({
-    words: ["Årets utställare på studentEXPO!"],
+    words: [titleText],
     onLoopDone: HandleDoneTyping,
   });
+
+  useEffect(() => {
+    let titleh1 = document.getElementById("titleText");
+    if (titleh1) {
+      titleh1.textContent = titleText;
+    }
+  }, [lang]);
+
   return (
     <>
       <Header
@@ -27,23 +41,34 @@ const StudentEXPO = () => {
       />
       <StarsCanvas />
       <div className="expo-title">
-        <h1>{Text}</h1>
+        <h1 id="titleText">{Text}</h1>
+        
       </div>
 
-      {doneTyping && (
+       {doneTyping && (
         <>
           <div className="expo-cards-container">
-            {Content.sv.uställare.map((utställare, index) => (
+            {/* {Content.sv.uställare.map((utställare, index) => (
               <ExpoCard
                 key={utställare}
                 index={index}
                 exhibitor={utställare}
-                delay={index / 5}
+                delay={index / 5}e
               />
-            ))}
+            ))} */}
+  
+            <h1 style={{
+              fontSize: "2rem",
+              color: "white",
+              textAlign: "center",
+              margin: "2rem",
+            }}>{lang === "sv" ? "Här kommer snart alla utställare på studentEXPO visas" : "All the exhibitors at studentEXPO will soon be shown here"}</h1>
+
           </div>
         </>
-      )}
+      )} 
+
+      
     </>
   );
 };
